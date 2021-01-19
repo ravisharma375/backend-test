74d999993773ef2499ee6253c97c130f616129a5
ravisharma375 <somethingfferent375@gmail.com>
2021-01-19 21:14:27 +0530

    new chenge

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var database = require("./config/database");
 var app = express();
-
+app.all("*", function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	next();
+});
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
